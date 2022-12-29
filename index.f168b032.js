ec0544096c700aa98ef8bb030b27bf171166ca55
SlipuyKamiN <117014063+SlipuyKamiN@users.noreply.github.com>
2022-12-29 08:15:08 +0000

    Deploying to gh-pages from @ SlipuyKamiN/Project-14@c097b5913547e97152c20b4402fb162d3a985e1c 🚀

---
+++ index.f168b032.js
@@ -0,0 +1,2 @@
+function changeHeaderBackground(){var e=document.querySelector("[data-header]"),n=e.offsetHeight;window.pageYOffset>n?e.classList.add("no-transparency"):e.classList.remove("no-transparency")}window.onscroll=function(){return changeHeaderBackground()};
+//# sourceMappingURL=index.f168b032.js.map
